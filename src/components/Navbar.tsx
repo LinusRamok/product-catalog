f47bae99e2988f2bcf92a0999d38aaa492023914
linusramok <linussunil.dsk@gmail.com>
2025-06-08 23:08:00 +0530

    fix: cart hydration error, added loading, error components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,21 @@
 "use client";
 import Link from "next/link";
 import { useCart } from "@/context/CartContext";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
   const { cartCount } = useCart();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  });
   return (
     <nav className="bg-white p-4 shadow flex justify-between">
       <Link href="/" className="font-bold text-lg">
         Product Catalog
       </Link>
-      <div>Cart ({cartCount})</div>
+      <div>Cart ({isMounted ? cartCount : "..."})</div>
     </nav>
   );
 };
