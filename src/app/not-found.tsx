2394076ab6527dfa0bda77b2034c2174363af184
linusramok <linussunil.dsk@gmail.com>
2025-06-08 23:08:00 +0530

    fix: cart hydration error, added loading, error components

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-3xl font-bold">404 - Page Not Found</h1>
+      <p className="text-gray-600">The page you're looking for doesn't exist.</p>
+    </div>
+  );
+}
+export default NotFound
